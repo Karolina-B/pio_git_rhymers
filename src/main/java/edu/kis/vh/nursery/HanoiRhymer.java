5dc4d162675dee86c3d2f7b79df5b33b968bcac9
Karolina <karolinabaczyk@o2.pl>
2022-01-06 18:08:20 +0100

    1.3.2 - poprawa błędów formatowania + wyjaśnienie skrótu klawiszowego w pliku 'HanoiRhymer'

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,21 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo()) totalRejected++;
+        else super.countIn(in);
+    }
 }
+
+//'if' oraz 'else' nie są w jednej linii
+//'if' można zapisać w jednej linii
+//'else' można zapisać w jednej linii
+//'brak wcięcia pzy zmiennej 'totalRejected'
+
+//'alt + <- lub alt + -> pozwala przełączać się między otwartymi plikami w projekcie
