3ebf9ec367a27ca5a3c45588bbd519708d7e82a4
Karolina-B <karolinabaczyk2000@gmail.com>
2022-01-21 17:31:46 +0100

    1.4 accessory 1 - poprawa atrybutów klas pod kątem widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int EMPTY = -1; //add final
-    public static final int NEGATIVE_TOTAL = -1; //add final
-    public static final int SIZE = 12;//add final
-    public static final int FULL = 11;//add final
+    private static final int EMPTY = -1; //add final
+    private static final int NEGATIVE_TOTAL = -1; //add final
+    private static final int SIZE = 12;//add final
+    private static final int FULL = 11;//add final
 
-    public int total = NEGATIVE_TOTAL;
+    private int total = NEGATIVE_TOTAL;
     private final int[] numbers = new int[SIZE];//add final
 
     public void countIn(int in) {
